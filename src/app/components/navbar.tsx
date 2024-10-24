acd7ba65638a2b1a439441b1106f1121ab80ff3c
wsmajt <kacpermusz@gmail.com>
2024-10-24 02:34:57 +0200

    + Logo ^ Navbar ^ Home Page

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,25 +1,48 @@
 ﻿import React from 'react'
-import Link from "next/link";
+import Logo from '../assets/smajtLogo.svg';
+import Image from "next/image";
 
 const navbar = () => {
   return (
       <div className="navbar bg-base-100">
-          <div className="flex-1">
-              <a className="btn btn-ghost text-primary font-bold text-xl">Blog - Next.js</a>
-              <ul className="menu menu-horizontal px-1 text-xl font-semibold">
-                  <li>
-                      <Link href="/">Link</Link>
-                  </li>
-                  <li>
-                      <Link href="/">Link</Link>
-                  </li>
-                  <li>
-                      <Link href="/">Link</Link>
-                  </li>
+          <div className="navbar-start">
+              <div className="dropdown">
+                  <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                      <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor">
+                          <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M4 6h16M4 12h8m-8 6h16"/>
+                      </svg>
+                  </div>
+                  <ul
+                      tabIndex={0}
+                      className="menu menu-lg dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow font-semibold text-2xl">
+                      <li><a>Posts</a></li>
+                      <li><a>Archive</a></li>
+                      <li><a>About</a></li>
+                  </ul>
+              </div>
+              <a className="btn btn-ghost font-bold text-2xl">
+                  <Image src={Logo} alt="SmajtLogo" className="h-18 w-8"/>
+                  <p className="">Blog - </p>
+                  <p className="text-primary">Next.js</p>
+              </a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1 font-semibold text-2xl">
+                  <li><a>Posts</a></li>
+                  <li><a>Archive</a></li>
+                  <li><a>About</a></li>
               </ul>
           </div>
-          <div className="flex-none">
-
+          <div className="navbar-end">
           </div>
       </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function Home() {
   return (
-      <main className="flex flex-col min-w-full min-h-screen items-center pt-10 px-60">
-          <div className="flex flex-col border border-base-300 rounded-t-3xl w-full items-center">
-              <h1 className="text-7xl font-bold text-primary">Lorem ipsum</h1>
+      <main className="flex flex-col min-w-full min-h-screen items-center pt-10 lg:px-60">
+          <div className="flex flex-col border border-base-300 rounded-t-3xl py-5 drop-shadow-xl shadow-xl w-full items-center">
+              <h1 className="text-4xl lg:text-7xl font-bold text-primary">Your Blog Page!</h1>
           </div>
       </main>
   );
